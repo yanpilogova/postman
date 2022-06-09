8f4659f7b698af86f99d4f421cebd8694846726c
yanpilogova <yulia.anpilogova@noveogroup.com>
2022-06-09 11:34:48 +0700

    added files

---
+++ src/test/java/ru/netology/ApiTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTest {
+    @Test
+    void shouldReturnData() {
+        // Given - When - Then
+        String s = "[0].id";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
